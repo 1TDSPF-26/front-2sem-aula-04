927694125bf9387cc7d21697dff27b4068b0be4d
AndrewRls <andrl57adf@gmail.com>
2026-08-12 12:04:16 -0300

    feat: meu primeiro componente integrado.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,12 @@
 /* Vamos criar um componente funcional */
 
+import Andrew from "./components/Nome/Andrew";
+
 export default function App(){
   return(
     <div>
       <h1>Olá mundo</h1>
+      <Andrew/>
     </div>
   );
 }
