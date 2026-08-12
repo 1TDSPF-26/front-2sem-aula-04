f245354d65444c9fe671c1a21ad242c5b0880abe
igorblacconaro <igorbs2910@gmail.com>
2026-08-12 12:05:39 -0300

    feat: Meu primeiro componente, integrado.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,13 @@
 //vamos criar um componente funcional
 
+import Igor from "./components/Nome/Igor";
+
 export default function App(){
   
   return(
     <div>
       <h1>Ola, mundo!</h1>
+      <Igor/>
     </div>
   );
 }
---
+++ my-app/src/components/Nome/Igor.tsx
@@ -0,0 +1,8 @@
+export default function Igor(){
+
+    return(
+        <>
+            <h2>Igor 00 da programação</h2>
+        </>
+    );
+}
