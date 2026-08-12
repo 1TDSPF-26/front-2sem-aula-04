9b143424d1b9dd86c54fef50992e20469b402353
lkoprowskif <larikoprowski@gmail.com>
2026-08-12 12:40:20 -0300

    feat: Merge branch 'develop' into develop.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-//Vamos criar um componente funcional
-
-export default function App() {
-
-  return(
-    <div>
-      <h1>Olá Mundo!</h1>
-=======
 
 import Ale from "./components/Nome/Ale";
 import Andrew from "./components/Nome/Andrew";
@@ -27,7 +18,6 @@ export default function App() {
       <Ga/>
       <Luiz/>
       <Pedro/>
->>>>>>> b25378b15e38cda5e3e978b7e01c7748d9459a60
     </div>
   );
 }
