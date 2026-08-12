c59c519938ae7255e33f3af63537b8cd6122a004
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-08-12 12:04:55 -0300

    feat : Meu primeiro componente integrado.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,13 @@
 //Vamos criar um componente funcional
 
+import Ale from "./components/Nome/Ale";
+
 export default function App() {
   
   return(
     <div>
       <h1>Olá Mundo!</h1>
+      <Ale/>
     </div>
   );
 }
