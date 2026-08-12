d3a38bb17dd0a363fed0f3a0b04f647a1e9d7b99
Pedro Lima Reis <outracontadopedro31@gmail.com>
2026-08-12 12:11:06 -0300

    feat: Integrando meu componente customizado ao App

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,12 @@
 //Vamos criar um componente funcional
 
+import Pedro from "./components/Nome/Pedro";
+
 export default function App() {
   return (
     <div>
-      <h1>Olá, mundo!</h1>
+      <h1>Olá Mundo!</h1>
+      <Pedro />
     </div>
   );
 }
