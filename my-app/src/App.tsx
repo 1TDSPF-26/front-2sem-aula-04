835d4dc0532a8565903eb09ae1640f3f29298e63
Kauã Silva de Jesus <kauansilva1465285@gmail.com>
2026-08-12 11:50:14 -0300

    Feat: Criado minha primeira estrutura

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+//Vamos criar um compornete funcional
+
+function App() {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>Como vai vc ?</p>
+      <h2>Direitos reservados</h2>
+    </div>
+  );
+}
+
+const nome = "João";
+console.log(nome);
+
+export default App; 
