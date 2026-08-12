b25378b15e38cda5e3e978b7e01c7748d9459a60
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-08-12 12:33:41 -0300

    feat: Todos na linha
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,23 @@
-<<<<<<< HEAD
-=======
 
+import Ale from "./components/Nome/Ale";
+import Andrew from "./components/Nome/Andrew";
+import Enzo from "./components/Nome/Enzo";
+import Ga from "./components/Nome/Ga";
+import Luiz from "./components/Nome/LuizEduardo";
 import Manu from "./components/Nome/Manu";
+import Pedro from "./components/Nome/Pedro";
+
 export default function App() {
   return(
     <div>
       <h1>Olá mundo</h1>
+      <Ale/>
+      <Andrew/>
       <Manu/>
+      <Enzo/>
+      <Ga/>
+      <Luiz/>
+      <Pedro/>
     </div>
   );
 }
-
->>>>>>> 2ec1e6f7b49b9cd41d2ffa91b34c1cb76e75ef54
