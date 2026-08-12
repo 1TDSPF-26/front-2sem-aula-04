f78c4293d7d376a7d5e84e0fcf03165c6ddb068e
enzoestanguera <enzo.estanguera@gmail.com>
2026-08-12 12:15:14 -0300

    feat: meu primeiro componente

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,11 @@
 //Vamos Criar um componente funcional 
 
+import Enzo from "./components/Nome/Enzo";
+
 export default function App() {
 
   return (<div>
         <h1>Olá mundo!</h1>
+        <Enzo/>
   </div>);
 }
