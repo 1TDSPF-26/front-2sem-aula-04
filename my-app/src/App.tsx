223e2759081a39fef2fce28ed8751fc01866dbc9
gabriel <gabrielrazod9j@gmail.com>
2026-08-17 15:50:21 -0300

    feat: meu primeiro componente integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+//Vamos criar um componente funcional
+
+import Ga from './components/Nome/Ga';
+
+export default function App() {
+    return(
+        <div>
+            <h1>Meu primeiro componente</h1>
+            <Ga />
+        </div>
+    );
+}
